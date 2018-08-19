0b33c3f70c3d8a999ec4e8e6f6c9c48f575ee034
Chris Keller <xylo04@gmail.com>
2018-08-19 17:03:03 -0600

    Add Material header bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatSliderModule, MatSlideToggleModule} from '@angular/material';
+import {
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule
+} from '@angular/material';
 import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {CalculatorComponent} from './calculator/calculator.component';
+import {AppNavComponent} from './app-nav/app-nav.component';
+import {LayoutModule} from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    AppNavComponent
   ],
   imports: [
     BrowserModule,
@@ -18,6 +29,12 @@ import {CalculatorComponent} from './calculator/calculator.component';
     FormsModule,
     MatSliderModule,
     MatSlideToggleModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
